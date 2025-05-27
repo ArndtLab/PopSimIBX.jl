4abfa7c732d0fbbf55b9abbd6161f9b6130b6d81
Documenter.jl <documenter@juliadocs.github.io>
2025-05-27 16:17:34 +0000

    build based on eedafb4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PopSimIBX","category":"page"},{"location":"#PopSimIBX","page":"Home","title":"PopSimIBX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PopSimIBX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PopSimIBX]","category":"page"}]
+[{"location":"#PopSimIBX","page":"Home","title":"PopSimIBX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PopSimIBX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }

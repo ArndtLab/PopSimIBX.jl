5a8b41184e96f592013a4f7a2a25a8081cd20a20
Documenter.jl <documenter@juliadocs.github.io>
2025-08-21 12:53:37 +0000

    build based on 41dd051

---
+++ v0.4.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PopSimIBX","page":"Home","title":"PopSimIBX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PopSimIBX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
